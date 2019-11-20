6a47e1809391cbb9df6166cbcbb6620ca5bdcedf
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-11-20 23:54:31 +0300

    Created cart model, product and cart get options models;

--- client-app/src/app/models/product.models.ts
+++ client-app/src/app/models/product.models.ts
@@ -1,7 +1,27 @@
-export class Product {
+import { BaseGetOptions } from './common.models';
+
+export interface Product {
     id: number;
-    name: string;
     price: number;
-    originalCount: number;
     count: number;
+    name: string;
+}
+
+export interface Cart extends Product {
+    id: number;
+    productId?: number;
+    orderId?: number;
+    price: number;
+    count: number;
+    name: string;
+}
+
+export class ProductGetOptions extends BaseGetOptions {
+    search?: string;
+}
+
+export interface CartGetOptions extends ProductGetOptions {
+    ordered?: boolean;
+    orderId?: number;
+    orderIds?: number[];
 }
