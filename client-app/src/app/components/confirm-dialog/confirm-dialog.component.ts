0df5c976e70ca7251018cb12543cc651bdb9e7f1
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-11-21 00:00:00 +0300

    Edited confirm dialog markup; Added able to show description in confirm dialog;

--- client-app/src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ client-app/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -8,7 +8,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ConfirmDialogComponent {
     constructor(
         public dialogRef: MatDialogRef<ConfirmDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: { message: string }) { }
+        @Inject(MAT_DIALOG_DATA) public data: { message: string, description?: string }) { }
 
     onCancelClick(): void {
         this.dialogRef.close();
