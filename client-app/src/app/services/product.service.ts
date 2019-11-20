afa56f4ffad8fd93b0afd5b39985e69c708d6681
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-11-20 23:51:46 +0300

    Replaced client methods by http requests;

--- client-app/src/app/services/product.service.ts
+++ client-app/src/app/services/product.service.ts
@@ -1,118 +1,41 @@
-import { Observable, BehaviorSubject } from 'rxjs';
-import { Product } from '../models/product.models';
+import { Observable } from 'rxjs';
+import { Product, ProductGetOptions } from '../models/product.models';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class ProductService {
-    private _products: Product[];
-    private _cart: Product[];
+    private readonly _apiUrl = 'api/product';
 
-    constructor() {
-        this._products = [];
-        this._cart = [];
+    private readonly _httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    constructor(private httpClient: HttpClient) { }
+
+    public get(options: ProductGetOptions): Observable<Product[]> {
+        const params = new HttpParams();
+        if (options.id)
+            params.set('id', options.id.toString());
+        if (options.ids)
+            params.set('ids', options.ids.toString());
+        if (options.search)
+            params.set('search', options.search);
+        return this.httpClient.get<Product[]>(this._apiUrl, { params });
     }
 
-    get products(): Product[] {
-        return this._products;
+    create(model: Product): Observable<Product> {
+        return this.httpClient.post<Product>(this._apiUrl, model, this._httpOptions);
     }
 
-    get cart(): Product[] {
-        return this._cart;
+    update(model: Product): Observable<Product> {
+        return this.httpClient.patch<Product>(this._apiUrl, model, this._httpOptions);
     }
 
-    get cartSummary(): number {
-        let summary = 0;
-        if (this._cart) {
-            summary = this._cart.reduce((sum, current) => sum + (current.price * current.count), 0);
-        }
-
-        return summary;
-    }
-
-    create(product: Product): void {
-        const id = Math.max(...this._products.map(o => o.id), 0) + 1;
-        const newProduct = {
-            ...product,
-            id: id,
-            originalCount: product.count
-        };
-        this._products.push(newProduct);
-    }
-
-    update(product: Product): void {
-        const oldProduct = this._products.find(o => o.id == product.id);
-        oldProduct.count = product.count;
-        oldProduct.name = product.name;
-        oldProduct.price = product.price;
-        oldProduct.originalCount = product.count;
-    }
-
-    delete(id: number): void {
-        this._products = this._products.splice(id, 1);
-    }
-
-    deleteFromCart(id: number): void {
-        this._cart = this._cart.filter(o => o.id !== id);
-        const product = this._products.find(o => o.id);
-        if (product)
-            product.count = product.originalCount;
-    }
-
-    setProductCount(id: number, count: number): Product {
-        const product = this._products.find(o => o.id == id);
-        product.count = product.originalCount - count;
-
-        return product;
-    }
-
-    setProductCountInCart(id: number, count: number): void {
-        const product = this.setProductCount(id, count);
-        let productInCart = this._cart.find(o => o.id == id);
-        if (productInCart) {
-            productInCart.count = count;
-        }
-        else {
-            productInCart = {
-                count: count,
-                id: product.id,
-                name: product.name,
-                price: product.price,
-                originalCount: product.originalCount
-            }
-
-            this._cart.push(productInCart);
-        }
-        if (productInCart.count <= 0) {
-            this._cart.splice(id, 1);
-        }
-    }
-
-    incrementProductCountInCart(id: number, count: number): void {
-        const product = this.decrementProductCount(id, count);
-        let productInCart = this._cart.find(o => o.id == id);
-        if (productInCart) {
-            productInCart.count += count;
-        }
-        else {
-            productInCart = {
-                count: count,
-                id: product.id,
-                name: product.name,
-                price: product.price,
-                originalCount: product.count
-            }
-
-            this._cart.push(productInCart);
-        }
-        if (productInCart.count <= 0) {
-            this._cart.splice(id, 1);
-        }
-    }
-
-    decrementProductCount(id: number, count: number): Product {
-        const product = this._products.find(o => o.id == id);
-        product.count -= count;
-
-        return product;
+    delete(ids: number[]): Observable<any> {
+        let url = `${this._apiUrl}?ids=${ids}`;
+        return this.httpClient.delete(url);
     }
 }
